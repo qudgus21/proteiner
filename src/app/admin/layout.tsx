57df544445947e42e8b3a371e17debc39e1e08f1
qudgus21 <qudgus21>
2024-09-16 14:26:11 +0700

    chore: layout 수정

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,25 +1,12 @@
-"use client";
-
-import React, { useState } from "react";
-import { useRouter } from "next/navigation";
+import React from "react";
 import Link from "next/link";
 
 const AdminLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [menuOpen, setMenuOpen] = useState(true);
-
   return (
-    <div className="flex">
-      <div className={`w-64 bg-gray-800 text-white ${menuOpen ? "block" : "hidden"} lg:block`}>
+    <div className="flex h-[100vh]">
+      <div className={`w-64 bg-blue-300 text-white block lg:block`}>
         <div className="p-4">
-          <button className="btn btn-primary w-full mb-4" onClick={() => setMenuOpen(!menuOpen)}>
-            {menuOpen ? "접기" : "펼치기"}
-          </button>
-          <ul className="menu">
-            <li>
-              <Link href="/admin/spreadsheet" className="menu-item">
-                스프레드시트 페이지
-              </Link>
-            </li>
+          <ul className="menu font-bold text-[15px] gap-[10px]">
             <li>
               <Link href="/admin/add-product" className="menu-item">
                 상품 등록 페이지
@@ -28,6 +15,11 @@ const AdminLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
             <li>
               <Link href="/admin/products" className="menu-item">
                 상품 리스트 페이지
+              </Link>
+            </li>
+            <li>
+              <Link href="/admin/spreadsheet" className="menu-item">
+                스프레드시트 페이지
               </Link>
             </li>
           </ul>
